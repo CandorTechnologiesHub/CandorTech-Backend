9782750da2de7c3146713087daf301cc8f0b7cc7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-03-18 10:40:05 +0800

    Setup

---
+++ src/test/java/com/candortech/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.candortech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
