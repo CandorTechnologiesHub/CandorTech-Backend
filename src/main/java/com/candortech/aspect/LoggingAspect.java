c7a4be9b38cd44355f4540b33d73b18f5a790836
INNOCENT UDO <innocentcharlesudo@gmail.com>
2026-03-19 21:27:22 +0800

    Log analysis

--- src/main/java/com/candortech/aspect/LoggingAspect.java
+++ src/main/java/com/candortech/aspect/LoggingAspect.java
@@ -1,4 +1,37 @@
 package com.candortech.aspect;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Before("execution(* com.candortech.aspect..*(..))")
+    public void logBeforeMethod(JoinPoint joinPoint) {
+        log.info("Enter: {}() with argument[s] = {}", joinPoint.getSignature().getName(), joinPoint.getArgs());
+    }
+
+    @AfterReturning(pointcut = "execution(* com.candortech.aspect..*(..))", returning = "result")
+    public void logAfterMethod(JoinPoint joinPoint, Object result) {
+        log.info("Exit: {}() with result = {}", joinPoint.getSignature().getName(), result);
+    }
+
+    @Around("execution(* com.candortech.aspect..*(..))")
+    public Object logAroundMethod(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long timeTaken = System.currentTimeMillis() - startTime;
+        log.info("Exit: {}() with result = {} and time taken = {} ms", joinPoint.getSignature().getName(), result, timeTaken);
+        return result;
+    }
 }
--- src/main/java/com/candortech/entity/UserProfile.java
+++ src/main/java/com/candortech/entity/UserProfile.java
@@ -1,5 +1,8 @@
 package com.candortech.entity;
 
+import jakarta.persistence.Entity;
+
+@Entity
 public class UserProfile {
     private long Id ;
 }
